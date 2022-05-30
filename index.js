ebd4c4f79645d6ed8888c72e86b0a9ce2a08a45e
Iwona Gorbacz <92158571+igorbacz@users.noreply.github.com>
2022-05-30 13:49:05 +0200

    not finished yet

--- index.js
+++ index.js
@@ -9,70 +9,90 @@ const usersFromJson = () => {
     {
       user: "user1",
       email: "aaa@user1.pl",
-      uri: "sdfghnbvfghjnbghj/fghj",
-      company: "Company 123",
+      uri: "/users/1",
+      company: "Company 2",
+      uris: { company: "/companies/2" },
     },
     {
       user: "user3",
       email: "aaa@user3.pl",
-      uri: "sdfghnbvfghjnbghj/fghj",
-      company: "Company 999",
+      uri: "/users/3",
+      company: "Company 2",
+      uris: { company: "/companies/2" },
     },
     {
       user: "user2",
       email: "aaa@user2.pl",
-      uri: "sdfghnbvfgh/fghj",
-      company: "Company 222",
+      uri: "/users/2",
+      company: "Company 0",
+      uris: { company: "/companies/0" },
     },
     {
       user: "user4",
       email: "aaa@user4.pl",
-      uri: "sdfghnbvfgh/fghj",
-      company: "Company 222",
+      uri: "/users/4",
+      company: "Company 1",
+      uris: { company: "/companies/1" },
     },
     {
       user: "user5",
       email: "aaa@user5.pl",
-      uri: "sdfghnbvfghjnbghj/fghj",
-      company: "Company 999",
+      uri: "/users/5",
+      company: "Company 0",
+      uris: { company: "/companies/0" },
     },
   ];
 
-  // console.log(data[1].user);
   const companies = [
-    { name: "Company 123", uri: "/companies/0" },
-    { name: "Company 999", uri: "/companies/1" },
-    { name: "Company 222", uri: "/companies/2" },
+    { name: "Company 0", uri: "/companies/0" },
+    { name: "Company 1", uri: "/companies/1" },
+    { name: "Company 2", uri: "/companies/2" },
   ];
 
   const findCompany = () => {
-    let dataEployees = [];
     for (let i = 0; i < data.length; i++) {
-      let companyName = data[i].company;
-      // find in array companies => companyName and ADD data[i] to companies[i].employees
+      let companyUris = data[i].uris.company;
       let employee = [data[i]][0];
-      let companyObject = companies.filter((x) => x.name === companyName);
 
-      let companyIndex = companies.findIndex((x) => x.name === companyName);
+      let companyObject = companies.filter((x) => x.uri === companyUris);
 
-      if (companies[companyIndex].name === companyName) {
-        dataEployees.push(employee);
-        companies[companyIndex].employees = dataEployees;
-      }
-
-      // console.log(companyIndex);
-
-      // console.log(companyObject);
-      // console.log(companyName);
+      let companyIndex = companies.findIndex((x) => x.uri === companyUris);
 
       console.log(employee);
+      console.log(companyObject[0]);
+      console.log(companyIndex);
+
+      if (employee.uris.company === companyObject[0].uri) {
+        let dataEployees = [];
+        companies[companyIndex].employees = dataEployees;3‹
+        console.log("match");
+        companyObject[0].employees = employee;
+      }
+
+      console.log(companies);
+
+      // console.log(companyObject[0]);
+      // if (employee) {
+      // }
+
+      // let companiesUsers = { ...companyObject[0], employee };
+      // console.log(companiesUsers);
+
+      // console.log(companies[companyIndex]);
+      // console.log(companyObject[0]);
+
+      // if (companies[companyIndex].uri === companyUris) {
+      // }
+
+      // console.log(companyIndex);   WORKS
+
+      // console.log(companyUris);    WORKS
+
       // console.log(dataEployees);
       // dataEployees.push(employee);
-      // companies[companyIndex].employees = dataEployees;
+      // // companies[companyIndex].employees = dataEployees;
 
-      console.log(companyObject);
-
-      // companyObject.employees = data[i];
+      // companyObject[0].employees = data[i];
 
       // companies.employees = data[i];
       // console.log(companies);
@@ -113,6 +133,6 @@ const usersFromJson = () => {
   const divShowData = document.getElementById("showData");
   divShowData.innerHTML = "";
   divShowData.appendChild(table);
-};;
+};
 
 document.addEventListener("DOMContentLoaded", usersFromJson());
